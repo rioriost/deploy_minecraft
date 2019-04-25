849be6e51aed0533f2b28faa9b726fc7c3c18ab0
Rio Fujita <rio_github@rio.st>
2019-04-25 21:11:41 +0900

    fix a typo

--- create_minecraft.sh
+++ create_minecraft.sh
@@ -54,7 +54,7 @@ if [ "$res" != "Succeeded" ]; then
 fi
 ipaddress=$(az container show -g $ACI_RES_GRP -n $ACI_CNT_NAME -o tsv --query "ipAddress.ip")
 
-echo <<< EOF
+echo << EOF
 Your Minecraft Container (${ipaddress}) has been successfully created!
 
 Command to stop the container:
